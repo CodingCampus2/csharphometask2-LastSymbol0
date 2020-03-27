76c22dfd32bb38a76fc237a938d49f81e00246f3
Illia Abrosimov <lastsymbol0@gmail.com>
2020-03-27 11:55:02 +0200

    dirty, but it works

--- Program.cs
+++ Program.cs
@@ -9,11 +9,17 @@ namespace HomeworkTemplate
         {
             Func<Task2, string> TaskSolver = task =>
             {
-                // Your solution goes here
-                // You can get all needed inputs from task.[Property]
-                // Good luck!
+                var enUSCulture = System.Globalization.CultureInfo.GetCultureInfo("en-US");
+                const int halfPadding = Task2.Padding / 2;
 
-                return "My answer";
+                FormattableString balanceMessage = $"{task.Balance:C}";
+                string balanceString = balanceMessage.ToString(enUSCulture);
+
+                balanceString = balanceString.PadLeft(halfPadding + balanceString.Length);
+                balanceString = balanceString.PadLeft(halfPadding + balanceString.Length, task.DecorativeSign);
+                balanceString = balanceString.PadRight(halfPadding + balanceString.Length);
+                balanceString = balanceString.PadRight(halfPadding + balanceString.Length, task.DecorativeSign);
+                return balanceString;
             };
 
             Task2.CheckSolver(TaskSolver);
